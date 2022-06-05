a5f0953db903abc8ffce08ac94b9fc0529597bf7
Mizogg <88630056+Mizogg@users.noreply.github.com>
2022-06-05 16:39:04 +0100

    Update Cbloom.py

--- WebserverV2/datafiles/Cbloom.py
+++ WebserverV2/datafiles/Cbloom.py
@@ -2,7 +2,22 @@
 # -*- coding: utf-8 -*-
 from bloomfilter import BloomFilter
 import sys
-bf = BloomFilter(size=line_count, fp_prob=1e-16)
+
+
+def bloom_filter():
+    print("[I] Bloom Filter START")
+    print("[I] File input -> " + file_txt)
+    print("[I] File output -> " + file_bf)
+    bf = BloomFilter(size=line_count, fp_prob=1e-16)
+
+    print("[I] ADD Bloom Filter")
+    add_to_bf(file_txt, line_count, bf)
+
+    print("[I] Bloom Filter Start Save File")
+    with open(file_bf, "wb") as fp:
+        bf.save(fp)
+    print("[I] Bloom Filter END Save File")
+
 
 def count_lines(file):
 	return sum(1 for line in open(file, 'r'))
@@ -27,11 +42,11 @@ def add_to_bf(file, nom, bf_filter):
 if __name__ == "__main__":
 
     if len (sys.argv) < 3:
-        print ("Mistake. Too few options.")
+        print ("Ошибка. Слишком мало параметров.")
         sys.exit (1)
 
     if len (sys.argv) > 3:
-        print ("Mistake. Too few options.")
+        print ("Ошибка. Слишком много параметров.")
         sys.exit (1)
 
     file_txt = sys.argv[1]
